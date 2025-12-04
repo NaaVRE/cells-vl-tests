63441f55ea0f1694af33064134bec47eebc0fb8e
skoulouzis <skoulouzis@users.noreply.github.com>
2025-12-04 11:23:22 +0100

    write-file-user

--- write-file-user/task.py
+++ write-file-user/task.py
@@ -18,12 +18,13 @@ id = args.id
 
 
 
-folder_path = '/home/jovyan/Cloud Storage/naa-vre-user-data/'
+folder = '/home/jovyan/Cloud Storage/naa-vre-user-data'
 filename = 'file.txt'
-path = os.path.join(folder_path, filename)
+path = os.path.join(folder, filename)
 
-with open(path, "a", encoding="utf-8") as f:
-    f.write("Appended line.\n")
+with open(path, "w", encoding="utf-8") as f:
+    f.write("Created using write mode.\n")
+    f.write("Second line.\n")
 
 file_path = open("/tmp/path_" + id + ".json", "w")
 file_path.write(json.dumps(path))
