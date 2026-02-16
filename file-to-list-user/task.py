133b32380584346dd52eda7e628731be75be9e65
skoulouzis <skoulouzis@users.noreply.github.com>
2026-02-16 15:46:01 +0200

    file-to-list-user

---
+++ file-to-list-user/task.py
@@ -0,0 +1,30 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--path', action='store', type=str, required=True, dest='path')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+path = args.path.replace('"','')
+
+
+
+lines = []
+with open(path) as file:
+    while line := file.readline():
+        lines.append(line.rstrip())
+
+file_lines = open("/tmp/lines_" + id + ".json", "w")
+file_lines.write(json.dumps(lines))
+file_lines.close()
