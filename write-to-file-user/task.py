c1cd7ab4818f072c0b836a0af9e1df0ea4020fae
skoulouzis <skoulouzis@users.noreply.github.com>
2026-02-16 17:27:07 +0200

    write-to-file-user

--- write-to-file-user/task.py
+++ write-to-file-user/task.py
@@ -21,21 +21,22 @@ conf_data_folder = conf_data_folder = os.path.join('/tmp', 'data')
 conf_user_folder = conf_user_folder = '/home/jovyan/Cloud Storage/naa-vre-user-data/'
 
 os.makedirs(conf_data_folder, exist_ok=True)
-filename = 'file.txt'
-path = os.path.join(conf_data_folder, filename)
 
-with open(path, "w", encoding="utf-8") as f:
+filename = 'hello.txt'
+file_path = os.path.join(conf_user_folder, filename)
+
+
+with open(file_path, "w", encoding="utf-8") as f:
     f.write("Created using write mode.\n")
     f.write("Second line.\n")
     f.write("Third line.\n")
 
-path = os.path.join(conf_user_folder, filename)
 
-with open(path, "w", encoding="utf-8") as f:
+with open(file_path, "w", encoding="utf-8") as f:
     f.write("Created using write mode.\n")
     f.write("Second line.\n")
     f.write("Third line.\n")
 
-file_path = open("/tmp/path_" + id + ".json", "w")
-file_path.write(json.dumps(path))
-file_path.close()
+file_file_path = open("/tmp/file_path_" + id + ".json", "w")
+file_file_path.write(json.dumps(file_path))
+file_file_path.close()
