241f3e9650cbd9ae1dfde3f4c0f97033f979f037
skoulouzis <skoulouzis@users.noreply.github.com>
2026-02-18 16:17:14 +0200

    print-user

--- print-user/task.py
+++ print-user/task.py
@@ -9,6 +9,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--param_with_dash', action='store', type=str, required=True, dest='param_with_dash')
 
 args = arg_parser.parse_args()
 print(args)
@@ -16,12 +17,14 @@ print(args)
 id = args.id
 
 
+param_with_dash = args.param_with_dash.replace('"','')
 
 conf_data_folder = conf_data_folder = os.path.join('/tmp', 'data')
 conf_user_folder = conf_user_folder = '/home/jovyan/Cloud Storage/naa-vre-user-data/'
 
 print(conf_data_folder)
 print(conf_user_folder)
+print(param_with_dash)
 
 int_val = 1
 int_type = 'int'
