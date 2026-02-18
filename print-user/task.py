0e276a599a6eb925b687c0e6380ce0b4c9fdf7ff
skoulouzis <skoulouzis@users.noreply.github.com>
2026-02-18 16:04:11 +0200

    print-user

--- print-user/task.py
+++ print-user/task.py
@@ -1,16 +1,13 @@
-import os
 
 import argparse
 import json
 import os
 arg_parser = argparse.ArgumentParser()
 
-secret_password = os.getenv('secret_password')
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--param_with_dash', action='store', type=str, required=True, dest='param_with_dash')
 
 args = arg_parser.parse_args()
 print(args)
@@ -18,15 +15,7 @@ print(args)
 id = args.id
 
 
-param_with_dash = args.param_with_dash.replace('"','')
 
-conf_data_folder = conf_data_folder = os.path.join('/tmp', 'data')
-conf_user_folder = conf_user_folder = '/home/jovyan/Cloud Storage/naa-vre-user-data/'
-
-print(conf_data_folder)
-print(conf_user_folder)
-print(param_with_dash)
-print(secret_password)
 
 int_val = 1
 int_type = 'int'
