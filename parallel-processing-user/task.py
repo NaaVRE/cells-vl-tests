b85d8528063d2bd81c8b7400b2b71853ff380e8a
skoulouzis <skoulouzis@users.noreply.github.com>
2026-02-20 16:21:29 +0200

    parallel-processing-user

--- parallel-processing-user/task.py
+++ parallel-processing-user/task.py
@@ -22,7 +22,8 @@ lines = json.loads(args.lines)
 
 new_lines = []
 for line in lines:
-    line += '_processed'
-    print(line)
-    new_lines.append(line)
+    new_lines.append(line+'new')
 
+file_new_lines = open("/tmp/new_lines_" + id + ".json", "w")
+file_new_lines.write(json.dumps(new_lines))
+file_new_lines.close()
