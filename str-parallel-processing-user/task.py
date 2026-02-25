aacee734d69ac238cb4dfb8eb64de831734ea011
skoulouzis <skoulouzis@users.noreply.github.com>
2026-02-25 15:10:41 +0200

    str-parallel-processing-user

---
+++ str-parallel-processing-user/task.py
@@ -0,0 +1,31 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--lines', action='store', type=str, required=True, dest='lines')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+lines = json.loads(args.lines)
+
+
+
+new_lines = []
+for line in lines:
+    line += '_processed'
+    print(line)
+    new_lines.append(line)
+
+file_new_lines = open("/tmp/new_lines_" + id + ".json", "w")
+file_new_lines.write(json.dumps(new_lines))
+file_new_lines.close()
