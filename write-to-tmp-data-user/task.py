f21c296408bdc2a125df972d53ed7ddf0cf15d7a
skoulouzis <skoulouzis@users.noreply.github.com>
2026-03-04 14:21:53 +0200

    write-to-tmp-data-user

---
+++ write-to-tmp-data-user/task.py
@@ -0,0 +1,33 @@
+import os
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+data_folder = os.path.join('/tmp', 'data')
+os.makedirs(data_folder, exist_ok=True)
+
+file_path = os.path.join(data_folder, 'file.txt')
+
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write("Created using write mode.\n")
+    f.write("Second line.\n")
+    f.write("Third line.\n")
+
+file_file_path = open("/tmp/file_path_" + id + ".json", "w")
+file_file_path.write(json.dumps(file_path))
+file_file_path.close()
