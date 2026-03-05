084fe2cdb670e27f9241143757fa600cfe715750
skoulouzis <skoulouzis@users.noreply.github.com>
2026-03-05 14:11:32 +0200

    input-list-user

---
+++ input-list-user/task.py
@@ -0,0 +1,24 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+str_list = ['a', 'b', 'c']
+
+file_str_list = open("/tmp/str_list_" + id + ".json", "w")
+file_str_list.write(json.dumps(str_list))
+file_str_list.close()
