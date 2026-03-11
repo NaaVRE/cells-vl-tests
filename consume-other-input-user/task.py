ec87c08fdce4b064282aeda8c5a8b67bc6c25002
skoulouzis <skoulouzis@users.noreply.github.com>
2026-03-11 16:45:51 +0200

    consume-other-input-user

---
+++ consume-other-input-user/task.py
@@ -0,0 +1,25 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--list2', action='store', type=str, required=True, dest='list2')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+list2 = json.loads(args.list2)
+
+
+
+for i in list2:
+    print(i)
+
