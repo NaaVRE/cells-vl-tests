8ec30e945a77acafc2f740d76be7c94e443fdb01
skoulouzis <skoulouzis@users.noreply.github.com>
2026-03-11 16:45:33 +0200

    consume-input-user

---
+++ consume-input-user/task.py
@@ -0,0 +1,25 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--list1', action='store', type=str, required=True, dest='list1')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+list1 = json.loads(args.list1)
+
+
+
+for i in list1:
+    print(i)
+
