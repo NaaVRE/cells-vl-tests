24fb2cbae08fe7c0a9cfc2d471511bfb2c431c48
skoulouzis <skoulouzis@users.noreply.github.com>
2026-03-16 17:28:49 +0200

    test_containerizeab39c0a3-f62a-4bf9-916a-770a45876673

---
+++ test_containerizeab39c0a3-f62a-4bf9-916a-770a45876673/task.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
+
+file_myvar = open("/tmp/myvar_" + id + ".json", "w")
+file_myvar.write(json.dumps(myvar))
+file_myvar.close()
