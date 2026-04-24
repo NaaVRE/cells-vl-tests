c1c8ecadc307a49d9620b41028c5074771f3efc8
skoulouzis <skoulouzis@users.noreply.github.com>
2026-04-24 13:58:22 +0300

    assert-new-lines-user

--- assert-new-lines-user/task.py
+++ assert-new-lines-user/task.py
@@ -20,5 +20,5 @@ new_lines = json.loads(args.new_lines)
 
 
 
-assert new_lines == ['a_processed', 'b_processed', 'c_processed', 'd_processed', 'e_processed', 'f_processed', 'g_processed', 'h_processed', 'i_processed', 'j_processed', 'k_processed', 'l_processed', 'm_processed', 'n_processed', 'o_processed', 'p_processed', 'q_processed', 'r_processed', 's_processed', 't_processed', 'u_processed', 'v_processed', 'w_processed', 'x_processed', 'y_processed', 'z_processed']
+assert new_lines == ['a_processed', 'b_processed', 'c_processed', 'd_processed', 'e_processed', 'f_processed', 'g_processed', 'h_processed', 'i_processed', 'j_processed', 'k_processed', 'l_processed', 'm_processed', 'n_processed', 'o_processed', 'p_processed', 'q_processed', 'r_processed']
 
