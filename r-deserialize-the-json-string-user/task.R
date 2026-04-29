f298a9811c8117d4a127130550cf3daaad75b282
skoulouzis <skoulouzis@users.noreply.github.com>
2026-04-29 23:16:15 +0300

    r-deserialize-the-json-string-user

---
+++ r-deserialize-the-json-string-user/task.R
@@ -0,0 +1,81 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("jsonlite", quietly = TRUE)) {
+	install.packages("jsonlite", repos="http://cran.us.r-project.org")
+}
+library(jsonlite)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--data"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--json_str"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--id"), action="store", default=NA, type="character", help="task id")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving data")
+var = opt$data
+print(var)
+var_len = length(var)
+print(paste("Variable data has length", var_len))
+
+print("------------------------Running var_serialization for data-----------------------")
+print(opt$data)
+data = var_serialization(opt$data)
+print("---------------------------------------------------------------------------------")
+
+print("Retrieving json_str")
+var = opt$json_str
+print(var)
+var_len = length(var)
+print(paste("Variable json_str has length", var_len))
+
+json_str <- gsub("\"", "", opt$json_str)
+id <- gsub('"', '', opt$id)
+
+
+print("Running the cell")
+deserialized_data <- fromJSON(json_str)
+
+print(deserialized_data)
+res <- all.equal(data, deserialized_data)
+
+if (!isTRUE(res)) {
+  stop(res)
+}
