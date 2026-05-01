ccde9554f62bf83dc8cbb232d98e9b741e720f09
skoulouzis <skoulouzis@users.noreply.github.com>
2026-05-01 20:44:50 +0300

    none-in-environment-yaml-user

--- none-in-environment-yaml-user/task.py
+++ none-in-environment-yaml-user/task.py
@@ -1,19 +1,54 @@
+# NaaVRE containerizer
+# version: V0.4
+
 import acolite
 
 import argparse
 import json
 import os
+
 arg_parser = argparse.ArgumentParser()
 
+def _load_json_args(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
 
-arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+def _parse_value(value, field_name, target_type, elem_type=None):
+    if target_type is list:
+        if isinstance(value, list):
+            return value
+        elif isinstance(value, str):
+            return json.loads(value)
+
+    if target_type is str:
+        return value
+
+    if target_type is int:
+        return int(value)
+
+    if target_type is float:
+        if isinstance(value, (int, float)):
+            return float(value)
+        if isinstance(value, str):
+            return float(value)
+
+    arg_parser.error(field_name + " has unsupported target type")
 
 
+
+arg_parser.add_argument('--args_json', action='store', type=str, required=True, dest='args_json')
 
 args = arg_parser.parse_args()
-print(args)
 
-id = args.id
+raw_args = _load_json_args(args.args_json)
+
+def get_arg_value(name):
+    for arg in raw_args:
+        if arg['name'] == name:
+            return arg['value']
+    arg_parser.error("Argument '" + name + "' not found in JSON args")
+
 
 
 
