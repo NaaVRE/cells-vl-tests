21f92c889c2370989e91149d025380f085bb8321
skoulouzis <skoulouzis@users.noreply.github.com>
2026-05-02 01:05:22 +0300

    create-file-r-user

--- create-file-r-user/task.R
+++ create-file-r-user/task.R
@@ -3,50 +3,56 @@ library(optparse)
 library(jsonlite)
 
 
-
-
-print('option_list')
-option_list = list(
-
-make_option(c("--id"), action="store", default=NA, type="character", help="task id")
-)
-
-
-opt = parse_args(OptionParser(option_list=option_list))
-
-var_serialization <- function(var){
-    if (is.null(var)){
-        print("Variable is null")
-        exit(1)
-    }
-    tryCatch(
-        {
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        error=function(e) {
-            print("Error while deserializing the variable")
-            print(var)
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        warning=function(w) {
-            print("Warning while deserializing the variable")
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        }
-    )
+load_json_args <- function(file_path) {
+  fromJSON(file_path, simplifyVector = FALSE)
 }
 
-id <- gsub('"', '', opt$id)
+parse_value <- function(value, field_name, target_type, elem_type = NULL) {
+  if (identical(target_type, "list")) {
+    if (is.list(value)) {
+      return(value)
+    } else if (is.character(value) && length(value) == 1) {
+      return(fromJSON(value, simplifyVector = FALSE))
+    }
+  }
+
+  if (identical(target_type, "str")) {
+    return(as.character(value))
+  }
+
+  if (identical(target_type, "int")) {
+    return(as.integer(value))
+  }
+
+  if (identical(target_type, "float")) {
+    if (is.numeric(value) || (is.character(value) && length(value) == 1)) {
+      return(as.numeric(value))
+    }
+  }
+
+  stop(paste0(field_name, " has unsupported target type"), call. = FALSE)
+}
+
+get_arg_value <- function(name, raw_args) {
+  for (arg in raw_args) {
+    if (!is.null(arg$name) && identical(arg$name, name)) {
+      return(arg$value)
+    }
+  }
+  stop(paste0("Argument '", name, "' not found in JSON args"), call. = FALSE)
+}
 
 
-print("Running the cell")
+option_list = list(
+make_option(c("--args_json"), action="store", default=NA, type="character", help="args json path")
+)
+
+opt = parse_args(OptionParser(option_list=option_list))
+raw_args = load_json_args(opt$args_json)
+
+
+
+
 L <- c("a", "b", "c")
 
 conf_data_folder <- "/tmp/data"
@@ -58,7 +64,6 @@ onlyfiles <- list.files(conf_data_folder, full.names = TRUE)
 
 print(onlyfiles)
 # capturing outputs
-print('Serialization of file_path')
 file <- file(paste0('/tmp/file_path_', id, '.json'))
 writeLines(toJSON(file_path, auto_unbox=TRUE), file)
 close(file)
