7f5d57ee2179f6ffc5dbe0c4030f8725addf214f
skoulouzis <skoulouzis@users.noreply.github.com>
2026-05-04 20:09:02 +0300

    s3-retiling-local-user

--- s3-retiling-local-user/task.py
+++ s3-retiling-local-user/task.py
@@ -1,3 +1,6 @@
+# NaaVRE containerizer
+# version: V0.4
+
 from laserfarm import Retiler
 import os
 import pathlib
@@ -5,30 +8,94 @@ import pathlib
 import argparse
 import json
 import os
+
+def _load_json_args(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+def _parse_value(value, field_name, target_type, elem_type=None):
+    if target_type is list:
+        if isinstance(value, list):
+            return value
+        elif isinstance(value, str):
+            try:
+                return json.loads(value)
+            except json.JSONDecodeError:
+                # the value may be "['list_str', 'space in elem', '3']"
+                if value.startswith('[') and value.endswith(']'):
+                    value = value[1:-1].strip()  # remove the brackets and trim whitespace
+                    if value:
+                        return [elem.strip().strip("'").strip('"') for elem in value.split(',')]
+                    else:
+                        return []
+                else:
+                    arg_parser.error(field_name + " is not a valid list")
+
+    if target_type is str:
+        return value
+
+    if target_type is int:
+        return int(value)
+
+    if target_type is float:
+        if isinstance(value, (int, float)):
+            return float(value)
+        if isinstance(value, str):
+            return float(value)
+    arg_parser.error(field_name + " has unsupported target type")
+
+def get_arg_value(name,args,raw_args):
+    for arg in raw_args:
+        if arg['name'] == name:
+            return arg['value']
+    arg_parser.error("Argument '" + name + "' not found in JSON args")
+
 arg_parser = argparse.ArgumentParser()
-
-
-arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
-
-
-arg_parser.add_argument('--split_laz_files', action='store', type=str, required=True, dest='split_laz_files')
-
+arg_parser.add_argument('--args_json', action='store', type=str, required=True, dest='args_json')
 
 args = arg_parser.parse_args()
-print(args)
 
-id = args.id
+expected_arg_names = set()
+expected_arg_names.add('split_laz_files')
+expected_arg_names.add('conf_local_path_retiled')
+expected_arg_names.add('conf_local_path_split')
+expected_arg_names.add('conf_max_x')
+expected_arg_names.add('conf_max_y')
+expected_arg_names.add('conf_min_x')
+expected_arg_names.add('conf_min_y')
+expected_arg_names.add('conf_n_tiles_side')
+expected_arg_names.add('id')
 
-split_laz_files = json.loads(args.split_laz_files)
+raw_args = _load_json_args(args.args_json)
+for arg in raw_args:
+    if 'name' not in arg:
+        arg_parser.error("Argument with no name found in JSON args")
+    if arg['name'] not in expected_arg_names:
+        arg_parser.error("Unexpected argument '" + arg['name'] + "' found in JSON args")
+    if not arg['name'].startswith('conf'):
+            if 'value' not in arg or arg['value'] is None:
+                arg_parser.error("Argument '" + arg['name'] + "' has no value in JSON args")
+    if arg['name'].startswith('conf'):
+            if 'assignation' not in arg or arg['assignation'] is None:
+                arg_parser.error("Argument '" + arg['name'] + "' has no assignation in JSON args")
+
+arg_name = 'split_laz_files'
+arg_value = get_arg_value(arg_name,args,raw_args)
+arg_type = list
+split_laz_files = _parse_value(arg_value, arg_name, arg_type)
+
+conf_local_path_retiled = os.path.join(pathlib.Path('/tmp/data').as_posix(), 'retiled')
+conf_local_path_split = os.path.join(pathlib.Path('/tmp/data').as_posix(), 'split')
+conf_max_x = '398892.19'
+conf_max_y = '726783.87'
+conf_min_x = '-113107.81'
+conf_min_y = '214783.87'
+conf_n_tiles_side = '512'
 
 
-conf_local_path_retiled = conf_local_path_retiled = os.path.join(pathlib.Path('/tmp/data').as_posix(), 'retiled')
-conf_local_path_split = conf_local_path_split = os.path.join(pathlib.Path('/tmp/data').as_posix(), 'split')
-conf_max_x = conf_max_x = '398892.19'
-conf_max_y = conf_max_y = '726783.87'
-conf_min_x = conf_min_x = '-113107.81'
-conf_min_y = conf_min_y = '214783.87'
-conf_n_tiles_side = conf_n_tiles_side = '512'
+arg_value = get_arg_value('id',args,raw_args)
+id = _parse_value(arg_value, 'id', str)
 
 split_laz_files
 
