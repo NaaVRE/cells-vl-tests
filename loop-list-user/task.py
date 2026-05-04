17641d2ef654bae8908d538219ad6e8abc16e75a
skoulouzis <skoulouzis@users.noreply.github.com>
2026-05-04 19:04:39 +0300

    loop-list-user

--- loop-list-user/task.py
+++ loop-list-user/task.py
@@ -1,24 +1,84 @@
+# NaaVRE containerizer
+# version: V0.4
+
 
 import argparse
 import json
 import os
+
+def _load_json_args(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+def _parse_value(value, field_name, target_type, elem_type=None):
+    if target_type is list:
+        if isinstance(value, list):
+            return value
+        elif isinstance(value, str):
+            try:
+                return json.loads(value)
+            except json.JSONDecodeError:
+                # the value may be "['list_str', 'space in elem', '3']"
+                if value.startswith('[') and value.endswith(']'):
+                    value = value[1:-1].strip()  # remove the brackets and trim whitespace
+                    if value:
+                        return [elem.strip().strip("'").strip('"') for elem in value.split(',')]
+                    else:
+                        return []
+                else:
+                    arg_parser.error(field_name + " is not a valid list")
+
+    if target_type is str:
+        return value
+
+    if target_type is int:
+        return int(value)
+
+    if target_type is float:
+        if isinstance(value, (int, float)):
+            return float(value)
+        if isinstance(value, str):
+            return float(value)
+    arg_parser.error(field_name + " has unsupported target type")
+
+def get_arg_value(name,args,raw_args):
+    for arg in raw_args:
+        if arg['name'] == name:
+            return arg['value']
+    arg_parser.error("Argument '" + name + "' not found in JSON args")
+
 arg_parser = argparse.ArgumentParser()
-
-
-arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
-
-
-arg_parser.add_argument('--list_of_paths', action='store', type=str, required=True, dest='list_of_paths')
-
+arg_parser.add_argument('--args_json', action='store', type=str, required=True, dest='args_json')
 
 args = arg_parser.parse_args()
-print(args)
 
-id = args.id
+expected_arg_names = set()
+expected_arg_names.add('list_of_paths')
+expected_arg_names.add('id')
 
-list_of_paths = json.loads(args.list_of_paths)
+raw_args = _load_json_args(args.args_json)
+for arg in raw_args:
+    if 'name' not in arg:
+        arg_parser.error("Argument with no name found in JSON args")
+    if arg['name'] not in expected_arg_names:
+        arg_parser.error("Unexpected argument '" + arg['name'] + "' found in JSON args")
+    if not arg['name'].startswith('conf'):
+            if 'value' not in arg or arg['value'] is None:
+                arg_parser.error("Argument '" + arg['name'] + "' has no value in JSON args")
+    if arg['name'].startswith('conf'):
+            if 'assignation' not in arg or arg['assignation'] is None:
+                arg_parser.error("Argument '" + arg['name'] + "' has no assignation in JSON args")
+
+arg_name = 'list_of_paths'
+arg_value = get_arg_value(arg_name,args,raw_args)
+arg_type = list
+list_of_paths = _parse_value(arg_value, arg_name, arg_type)
 
 
+
+arg_value = get_arg_value('id',args,raw_args)
+id = _parse_value(arg_value, 'id', str)
 
 for l in list_of_paths:
     print(l)
