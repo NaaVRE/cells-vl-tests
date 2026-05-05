403001a4902b1b29f02037f8075f08d089cbe3e1
skoulouzis <skoulouzis@users.noreply.github.com>
2026-05-05 09:55:45 +0300

    secret-reader-user

--- secret-reader-user/task.py
+++ secret-reader-user/task.py
@@ -1,90 +1,27 @@
-# NaaVRE containerizer
-# version: V0.4
-
 
 import argparse
 import json
 import os
-
-def _load_json_args(file_path):
-    with open(file_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    return data
-
-def _parse_value(value, field_name, target_type, elem_type=None):
-    if target_type is list:
-        if isinstance(value, list):
-            return value
-        elif isinstance(value, str):
-            try:
-                return json.loads(value)
-            except json.JSONDecodeError:
-                # the value may be "['list_str', 'space in elem', '3']"
-                if value.startswith('[') and value.endswith(']'):
-                    value = value[1:-1].strip()  # remove the brackets and trim whitespace
-                    if value:
-                        return [elem.strip().strip("'").strip('"') for elem in value.split(',')]
-                    else:
-                        return []
-                else:
-                    arg_parser.error(field_name + " is not a valid list")
-
-    if target_type is str:
-        return value
-
-    if target_type is int:
-        return int(value)
-
-    if target_type is float:
-        if isinstance(value, (int, float)):
-            return float(value)
-        if isinstance(value, str):
-            return float(value)
-    arg_parser.error(field_name + " has unsupported target type")
-
-def get_arg_value(name,args,raw_args):
-    for arg in raw_args:
-        if arg['name'] == name:
-            return arg['value']
-    arg_parser.error("Argument '" + name + "' not found in JSON args")
-
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('--args_json', action='store', type=str, required=True, dest='args_json')
-
-args = arg_parser.parse_args()
-
-expected_arg_names = set()
-expected_arg_names.add('foo')
-expected_arg_names.add('param_abc')
-expected_arg_names.add('id')
-
-raw_args = _load_json_args(args.args_json)
-for arg in raw_args:
-    if 'name' not in arg:
-        arg_parser.error("Argument with no name found in JSON args")
-    if arg['name'] not in expected_arg_names:
-        arg_parser.error("Unexpected argument '" + arg['name'] + "' found in JSON args")
-    if not arg['name'].startswith('conf'):
-            if 'value' not in arg or arg['value'] is None:
-                arg_parser.error("Argument '" + arg['name'] + "' has no value in JSON args")
-    if arg['name'].startswith('conf'):
-            if 'assignation' not in arg or arg['assignation'] is None:
-                arg_parser.error("Argument '" + arg['name'] + "' has no assignation in JSON args")
-
-arg_name = 'foo'
-arg_value = get_arg_value(arg_name,args,raw_args)
-arg_type = str
-foo = _parse_value(arg_value, arg_name, arg_type)
-arg_name = 'param_abc'
-arg_value = get_arg_value(arg_name,args,raw_args)
-arg_type = str
-param_abc = _parse_value(arg_value, arg_name, arg_type)
-
 
 secret_xyz = os.getenv('secret_xyz')
 
-arg_value = get_arg_value('id',args,raw_args)
-id = _parse_value(arg_value, 'id', str)
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--foo', action='store', type=str, required=True, dest='foo')
+
+arg_parser.add_argument('--param_abc', action='store', type=str, required=True, dest='param_abc')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+foo = args.foo.replace('"','')
+
+param_abc = args.param_abc.replace('"','')
+
 
 print(param_abc, secret_xyz, foo)
 
