a8babadf22c29f7afe57c4269f6d821925d0c331
skoulouzis <skoulouzis@users.noreply.github.com>
2026-09-04 14:18:30 +0200

    test-cell-1-user

--- test-cell-1-user/task.py
+++ test-cell-1-user/task.py
@@ -1,3 +1,4 @@
+import csv
 
 import argparse
 import json
@@ -18,10 +19,21 @@ id = args.id
 
 
 secret_test_1 = 'secret'
-test_value_1 = [1.2]
+test_value_1 = [1,2]
 
-fdo_output = 'fdo_data'
 
+fdo_output = '/tmp/data/fdo_data.csv'
+
+
+with open(fdo_output, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["value"])       # header
+    for item in test_value_1:
+        writer.writerow([item])
+
+file_fdo_output = open("/tmp/fdo_output_" + id + ".json", "w")
+file_fdo_output.write(json.dumps(fdo_output))
+file_fdo_output.close()
 file_test_value_1 = open("/tmp/test_value_1_" + id + ".json", "w")
 file_test_value_1.write(json.dumps(test_value_1))
 file_test_value_1.close()
