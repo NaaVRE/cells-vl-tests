57827e29908d4fb7726082979a59588eaf6d7947
skoulouzis <skoulouzis@users.noreply.github.com>
2026-09-10 14:45:20 +0200

    r-check-var-types-user

--- r-check-var-types-user/task.R
+++ r-check-var-types-user/task.R
@@ -75,8 +75,10 @@ print(var)
 var_len = length(var)
 print(paste("Variable param_list_int has length", var_len))
 
+print("------------------------Running var_serialization for param_list_int-----------------------")
 print(opt$param_list_int)
 param_list_int = var_serialization(opt$param_list_int)
+print("---------------------------------------------------------------------------------")
 
 print("Retrieving param_list_str")
 var = opt$param_list_str
@@ -84,8 +86,10 @@ print(var)
 var_len = length(var)
 print(paste("Variable param_list_str has length", var_len))
 
+print("------------------------Running var_serialization for param_list_str-----------------------")
 print(opt$param_list_str)
 param_list_str = var_serialization(opt$param_list_str)
+print("---------------------------------------------------------------------------------")
 
 print("Retrieving param_string")
 var = opt$param_string
@@ -121,8 +125,10 @@ print(var)
 var_len = length(var)
 print(paste("Variable var_list_int has length", var_len))
 
+print("------------------------Running var_serialization for var_list_int-----------------------")
 print(opt$var_list_int)
 var_list_int = var_serialization(opt$var_list_int)
+print("---------------------------------------------------------------------------------")
 
 print("Retrieving var_list_str")
 var = opt$var_list_str
@@ -130,8 +136,10 @@ print(var)
 var_len = length(var)
 print(paste("Variable var_list_str has length", var_len))
 
+print("------------------------Running var_serialization for var_list_str-----------------------")
 print(opt$var_list_str)
 var_list_str = var_serialization(opt$var_list_str)
+print("---------------------------------------------------------------------------------")
 
 print("Retrieving var_string")
 var = opt$var_string
